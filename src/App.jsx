20fa1dfb8b38b193c1eb40a984c21b71123d25be
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-11-10 09:14:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+
+
+const App = () => {
+  return (
+    <>
+      <Home/>
+      <About/>
+      <Contact/>
+      <Products/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Products.jsx
@@ -0,0 +1,9 @@
+const Products = () => {
+	return (
+		<div>
+			<h2>Wellcome to Products Page</h2>
+		</div>
+	);
+};
+
+export default Products;
